dba8d0759c7ba415c2da1dc0f623726ea27fe151
Irene Riportella <riportellairene@gmail.com>
2025-05-10 18:00:49 +0200

    feat:✨ create Video component

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
 
-function Video({src}) {
-  return <div>
-    <video src={src} loop autoPlay controls></video>
-  </div>;
+function Video({ src }) {
+  return (
+    <div>
+      <video
+        src={src}
+        loop
+        autoPlay
+        controls
+        className="w-full h-full object-contain"
+      ></video>
+    </div>
+  );
 }
 
 export default Video;
