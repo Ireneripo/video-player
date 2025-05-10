79748af30adc916688c0f6f4836453e7ac3a588a
Irene Riportella <riportellairene@gmail.com>
2025-05-10 18:00:37 +0200

    feat:✨ create Menu component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React from "react";
 
-function Menu() {
-  return <div></div>;
+function Menu({ sourceOptions, onSelectVideo }) {
+  const handleChange = (event) => {
+    onSelectVideo(event.target.value);
+  };
+
+  console.log(Object.entries(sourceOptions));
+
+  return (
+    <form className="mb-4 flex gap-4 justify-center">
+      {Object.entries(sourceOptions).map(([name, url], index) => (
+        <div
+          key={index}
+          className="flex items-center"
+        >
+          <input
+            id={`video-${index}`}
+            type="radio"
+            name="video"
+            value={url}
+            onChange={handleChange}
+            className="mr-2"
+          />
+          <label htmlFor={`video-${index}`}>{name}</label>
+        </div>
+      ))}
+    </form>
+  );
 }
 
 export default Menu;
